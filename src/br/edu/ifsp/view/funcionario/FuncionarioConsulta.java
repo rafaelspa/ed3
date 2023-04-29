96f44c582185686e35421b2c1d4e9c6b0b8b846c
rafaelspa <rsdepaula67@gmail.com>
2023-04-29 10:22:58 -0300

    A9: Atualizando a FuncionarioConsulta

--- src/br/edu/ifsp/view/funcionario/FuncionarioConsulta.java
+++ src/br/edu/ifsp/view/funcionario/FuncionarioConsulta.java
@@ -2,11 +2,14 @@ package br.edu.ifsp.view.funcionario;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import br.edu.ifsp.controller.FuncionarioController;
 import br.edu.ifsp.model.funcionario.Funcionario;
 
 public class FuncionarioConsulta {
+	
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void exibeInterface() {
 		System.out.println("\nFUNCIONARIOS CADASTRADOS:");
@@ -21,9 +24,39 @@ public class FuncionarioConsulta {
 		}
 		funcionarios.forEach(funcionario -> {
 			System.out.println(funcionario.getId() + " - " + funcionario.getNome() + " - " + funcionario.getSexo()
-					+ " - " + (funcionario.isPlanoSaude() ? "Tem" : "Nao tem")
+					+ " - " + (funcionario.isPlanoSaude() ? "Tem" : "Nao tem") + " - "
 					+ (funcionario.getCargo() != null ? funcionario.getCargo().getDescricao() : "Sem cargo"));
 		});
 		System.out.println();
+
+		exibeOpcoesAlterarExcluir();
+	}
+
+	public static void exibeOpcoesAlterarExcluir() {
+		int opcao = -1;
+		while (opcao != 0) {
+			System.out.println("ALTERACAO / EXCLUSAO DE FUNCIONARIO:");
+			System.out.println("1) Alterar");
+			System.out.println("2) Excluir");
+			System.out.print("Digite uma opcao (0 para voltar): ");
+			opcao = scanner.nextInt();
+			System.out.println();
+			switch (opcao) {
+				case 0: {
+					return;
+				}
+				case 1: {
+					FuncionarioAlteracao.exibeInterface();
+					
+					break;
+				}
+				case 2: {
+//					FuncionarioExclusao.exibeInterface();
+					break;
+				} default:
+					System.out.print("\nDigite uma opcao valida. ");
+					System.out.println();
+				}
+			}
 	}
 }
